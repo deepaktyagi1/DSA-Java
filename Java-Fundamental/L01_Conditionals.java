bfb7f47bb7a7b5410cc23e80936ccb30237eef70
Deepak Tyagi <160519381+deepaktyagi1@users.noreply.github.com>
2026-08-29 15:26:38 +0530

    Update L01_Conditionals.java

--- Java-Fundamental/L01_Conditionals.java
+++ Java-Fundamental/L01_Conditionals.java
@@ -6,130 +6,130 @@ public class L01_Conditionals {
         
 //   --- IF-STATEMENT ---
     
-// int dailyPractice = 12;
+int dailyPractice = 12;
 
-// if (dailyPractice >=10) {
-//     System.out.println("Good Consisteny!");
-// }
+if (dailyPractice >=10) {
+    System.out.println("Good Consisteny!");
+}
 
-// int age =100;
+int age =100;
 
-// if (age>18) {
-//     System.out.println("You are eligible to vote");
-// }
+if (age>18) {
+    System.out.println("You are eligible to vote");
+}
 
 
 //   --- IF-ELSE STATEMENT ---
 
-// int score = 93;
+int score = 93;
 
-// if(score>=50) {
-//     System.out.println("Pass");
-// } else {
-//     System.out.println("Fail");
-// }
+if(score>=50) {
+    System.out.println("Pass");
+} else {
+    System.out.println("Fail");
+}
 
-// int age =20;
-// if(age>=18){
-//     System.out.println("Can vote");
-// } else {System.out.println("Cannot vote");}
+int age =20;
+if(age>=18){
+    System.out.println("Can vote");
+} else {System.out.println("Cannot vote");}
 
 
 //   --- IF-ELSE-IF Ladder ---
 
-// int accuracy = 97;
+int accuracy = 97;
 
-// if(accuracy>=90){
-//     System.out.println("Excelent");
-// }
-// else if(accuracy>=75){
-//     System.out.println("Good");
-// }
-// else if(accuracy>=60){
-//     System.out.println("average");
-// } else{System.out.println("Needs Improvement");}
+if(accuracy>=90){
+    System.out.println("Excelent");
+}
+else if(accuracy>=75){
+    System.out.println("Good");
+}
+else if(accuracy>=60){
+    System.out.println("average");
+} else{System.out.println("Needs Improvement");}
 
-// int day = 2;
+int day = 2;
 
-// if(day==1){
-//     System.out.println("Monday");
-// }
-// else if(day==2){
-//     System.out.println("Tuesday");
-// }
-// else if(day==3){
-//     System.out.println("Wednesday");
-// }
-// else if(day==4){
-//     System.out.println("Thursday");
-// }
-// else if(day==5){
-//     System.out.println("Friday");
-// }
-// else if(day==6){
-//     System.out.println("Saturday");
-// } else {
-//     System.out.println("Sunday");
-// }
+if(day==1){
+    System.out.println("Monday");
+}
+else if(day==2){
+    System.out.println("Tuesday");
+}
+else if(day==3){
+    System.out.println("Wednesday");
+}
+else if(day==4){
+    System.out.println("Thursday");
+}
+else if(day==5){
+    System.out.println("Friday");
+}
+else if(day==6){
+    System.out.println("Saturday");
+} else {
+    System.out.println("Sunday");
+}
 
 
    //    --- Nested-IF-ELSE---
 
-//    boolean hasSubscription = true;
-//    int solvedproblems = 220;
+   boolean hasSubscription = true;
+   int solvedproblems = 220;
 
-//    if(hasSubscription){
-//     if(solvedproblems>=220){
-//         System.out.println("Unlock Advance Sheet");
-//     } else{System.out.println("Practice More Problems");}
-//    }else{System.out.println("Upgrade to Premium");}
+   if(hasSubscription){
+    if(solvedproblems>=220){
+        System.out.println("Unlock Advance Sheet");
+    } else{System.out.println("Practice More Problems");}
+   }else{System.out.println("Upgrade to Premium");}
 
-    //   char gender ='F';
-    //   int age =19;
+      char gender ='F';
+      int age =19;
 
-    //   if(gender=='M'){
-    //     if(age>=18){
-    //         System.out.println("You are male and age>18");
-    //     }else{System.out.println("You are male and age<18");}
-    //   }
-    //   else {
-    //     if(age>=18) {
-    //         System.out.println("You are not male and age>18");
-    //     } else {System.out.println("You are not male and age<18");}
-    //   }
+      if(gender=='M'){
+        if(age>=18){
+            System.out.println("You are male and age>18");
+        }else{System.out.println("You are male and age<18");}
+      }
+      else {
+        if(age>=18) {
+            System.out.println("You are not male and age>18");
+        } else {System.out.println("You are not male and age<18");}
+      }
 
 
     //   --- Ternary Operator ---
 
-    //   int streakDays=55;
+      int streakDays=55;
 
-    //   String status = (streakDays>30) ? "Consistency" : "Irregular";
-    //   System.out.println(status);
+      String status = (streakDays>30) ? "Consistency" : "Irregular";
+      System.out.println(status);
 
 
     //     --- Switch Statement ---
 
-    // System.out.print("Enter Value of Day :");
+    System.out.print("Enter Value of Day :");
 
-    // Scanner sc = new Scanner(System.in);
+    Scanner sc = new Scanner(System.in);
 
-    // int day = sc.nextInt();
+    int day = sc.nextInt();
 
-    // switch (day) {
-    //     case 1: System.out.println("Monday");
-    //         break;
-    //     case 2: System.out.println("Tuesday");
-    //         break;
-    //     case 3: System.out.println("Wednesday");
-    //         break;
-    //     case 4: System.out.println("Thursday");
-    //         break;
-    //     case 5: System.out.println("Friday");
-    //         break;
-    //     case 6: System.out.println("Saturday");
-    //         break;
-    //     default: System.out.println("Sunday");
-    // }
+    switch (day) {
+        case 1: System.out.println("Monday");
+            break;
+        case 2: System.out.println("Tuesday");
+            break;
+        case 3: System.out.println("Wednesday");
+            break;
+        case 4: System.out.println("Thursday");
+            break;
+        case 5: System.out.println("Friday");
+            break;
+        case 6: System.out.println("Saturday");
+            break;
+        default: System.out.println("Sunday");
+    }
 
     // sc.close();
     }
